ee68c6ea3511bbb87b32bac00c2eb5aa149a5c08
Peter Bräuer <pb866@york.ac.uk>
2018-11-13 23:32:29 +0000

    Deployed c9abe29 with MkDocs version: 1.0.4

--- search_index.js
+++ search_index.js
@@ -13,39 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Test the auto-generation of Documentation with Documenter.jl",
     "title": "Test the auto-generation of Documentation with Documenter.jl",
     "category": "section",
-    "text": ""
-},
-
-{
-    "location": "index.html#testdocs.greet",
-    "page": "Test the auto-generation of Documentation with Documenter.jl",
-    "title": "testdocs.greet",
-    "category": "function",
-    "text": "greet(name::String=\"Peter\")\n\nPrint a greeting to the specified name.\n\n\n\n\n\n"
-},
-
-{
-    "location": "index.html#This-is-the-first-fucntion-1",
-    "page": "Test the auto-generation of Documentation with Documenter.jl",
-    "title": "This is the first fucntion",
-    "category": "section",
-    "text": "It is completely meaningless.greet(name::String=\"Peter\")The other stupid function add_numbers(a::Number, b::Number) can be found in Sect. This is the second function."
-},
-
-{
-    "location": "index.html#testdocs.add_numbers-Tuple{Number,Number}",
-    "page": "Test the auto-generation of Documentation with Documenter.jl",
-    "title": "testdocs.add_numbers",
-    "category": "method",
-    "text": "add_numbers(a::Number, b::Number)\n\nAdd two numbers a and b.\n\n\n\n\n\n"
-},
-
-{
-    "location": "index.html#This-is-the-second-function-1",
-    "page": "Test the auto-generation of Documentation with Documenter.jl",
-    "title": "This is the second function",
-    "category": "section",
-    "text": "Again utter nonsense.add_numbers(a::Number, b::Number)"
+    "text": "Pages = [\"page1.md\", \"page2.md\"]\nDepth = 3"
 },
 
 {
@@ -54,6 +22,70 @@ var documenterSearchIndex = {"docs": [
     "title": "Index",
     "category": "section",
     "text": ""
+},
+
+{
+    "location": "page1.html#",
+    "page": "Original Functions",
+    "title": "Original Functions",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "page1.html#testdocs.greet",
+    "page": "Original Functions",
+    "title": "testdocs.greet",
+    "category": "function",
+    "text": "greet(name::String=\"Peter\")\n\nPrint a greeting to the specified name.\n\n\n\n\n\n"
+},
+
+{
+    "location": "page1.html#This-is-the-first-fucntion-1",
+    "page": "Original Functions",
+    "title": "This is the first fucntion",
+    "category": "section",
+    "text": "It is completely meaningless.greet(name::String=\"Peter\")The other stupid function add_numbers(a::Number, b::Number) can be found in Sect. This is the second function."
+},
+
+{
+    "location": "page1.html#testdocs.add_numbers-Tuple{Number,Number}",
+    "page": "Original Functions",
+    "title": "testdocs.add_numbers",
+    "category": "method",
+    "text": "add_numbers(a::Number, b::Number)\n\nAdd two numbers a and b.\n\n\n\n\n\n"
+},
+
+{
+    "location": "page1.html#This-is-the-second-function-1",
+    "page": "Original Functions",
+    "title": "This is the second function",
+    "category": "section",
+    "text": "Again utter nonsense.add_numbers(a::Number, b::Number)"
+},
+
+{
+    "location": "page2.html#",
+    "page": "Added Function",
+    "title": "Added Function",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "page2.html#testdocs.version2func-Tuple{VersionNumber}",
+    "page": "Added Function",
+    "title": "testdocs.version2func",
+    "category": "method",
+    "text": "version2func(version::VersionNumber)\n\nPrints the version number.\n\n\n\n\n\n"
+},
+
+{
+    "location": "page2.html#This-function-has-been-added-later-1",
+    "page": "Added Function",
+    "title": "This function has been added later",
+    "category": "section",
+    "text": "It can do no more, but print a version number you give it.testdocs.version2func(version::VersionNumber)"
 },
 
 ]}
